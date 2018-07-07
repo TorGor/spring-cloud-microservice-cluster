a16716bfd123ab118f36d49654bed5f1b7947cc9
holy <holyplace@163.com>
2018-07-07 19:19:20 +0800

    update hystrix client /hystrix.stream url,you can use "management.endpoints.web.exposure.include: hystrix.stream" simple to exposes the /actuator/hystrix.stream  and instand of /hystrix.stream

--- eureka-client-invoker/src/main/java/com/bolocloud/eurekaclientcall/config/MyConfig.java
+++ eureka-client-invoker/src/main/java/com/bolocloud/eurekaclientcall/config/MyConfig.java
@@ -14,15 +14,15 @@ public class MyConfig {
         return new DefineRibbonRule();
     }
 
-    @Bean
-    public ServletRegistrationBean getServlet(){
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/actuator/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
-    }
+//    @Bean
+//    public ServletRegistrationBean getServlet(){
+//        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+//        registrationBean.setLoadOnStartup(1);
+//        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+//        registrationBean.setName("HystrixMetricsStreamServlet");
+//        return registrationBean;
+//    }
 
 
 }
